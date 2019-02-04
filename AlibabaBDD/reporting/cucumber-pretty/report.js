acf435d6952f38f9b4234ef2474a1aa1fb37afc9
mkhan1231 <mahmud8118@gmail.com>
2019-02-04 03:18:23 -0500

    BDD Framework

--- AlibabaBDD/reporting/cucumber-pretty/report.js
+++ AlibabaBDD/reporting/cucumber-pretty/report.js
@@ -2,7 +2,7 @@ $(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.
 formatter.feature({
   "line": 2,
   "name": "Search Feature",
-  "description": "In order to search item in search product\nas a user I need to be in Home Page",
+  "description": "In order to search item in search product\r\nas a user I need to be in Home Page",
   "id": "search-feature",
   "keyword": "Feature",
   "tags": [
@@ -88,7 +88,7 @@ formatter.examples({
   "keyword": "Examples"
 });
 formatter.before({
-  "duration": 4397050496,
+  "duration": 21381988914,
   "status": "passed"
 });
 formatter.scenario({
@@ -133,7 +133,7 @@ formatter.match({
   "location": "SearchSteps.i_am_in_Home_Page()"
 });
 formatter.result({
-  "duration": 92838404,
+  "duration": 1473064746,
   "status": "passed"
 });
 formatter.match({
@@ -150,29 +150,28 @@ formatter.match({
   "location": "SearchSteps.enter_product_name_in_search_box(String,String)"
 });
 formatter.result({
-  "duration": 3288435375,
-  "status": "passed"
+  "duration": 911510898,
+  "error_message": "org.openqa.selenium.WebDriverException: unknown error: call function result missing \u0027value\u0027\n  (Session info: chrome\u003d71.0.3578.98)\n  (Driver info: chromedriver\u003d2.34.522940 (1a76f96f66e3ca7b8e57d503b4dd3bccfba87af1),platform\u003dWindows NT 10.0.17134 x86_64) (WARNING: The server did not provide any stacktrace information)\nCommand duration or timeout: 880 milliseconds\nBuild info: version: \u00273.0.1\u0027, revision: \u00271969d75\u0027, time: \u00272016-10-18 09:49:13 -0700\u0027\nSystem info: host: \u0027DESKTOP-LCVS2C2\u0027, ip: \u0027192.168.131.2\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_191\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities [{applicationCacheEnabled\u003dfalse, rotatable\u003dfalse, mobileEmulationEnabled\u003dfalse, networkConnectionEnabled\u003dfalse, chrome\u003d{chromedriverVersion\u003d2.34.522940 (1a76f96f66e3ca7b8e57d503b4dd3bccfba87af1), userDataDir\u003dC:\\Users\\MAHMUD~1\\AppData\\Local\\Temp\\scoped_dir21116_2599}, takesHeapSnapshot\u003dtrue, pageLoadStrategy\u003dnormal, databaseEnabled\u003dfalse, handlesAlerts\u003dtrue, hasTouchScreen\u003dfalse, version\u003d71.0.3578.98, platform\u003dXP, browserConnectionEnabled\u003dfalse, nativeEvents\u003dtrue, acceptSslCerts\u003dtrue, locationContextEnabled\u003dtrue, webStorageEnabled\u003dtrue, browserName\u003dchrome, takesScreenshot\u003dtrue, javascriptEnabled\u003dtrue, cssSelectorsEnabled\u003dtrue, setWindowRect\u003dtrue, unexpectedAlertBehaviour\u003d}]\nSession ID: 5810e20ba13ddf9fe24bc94840f9f369\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.openqa.selenium.remote.ErrorHandler.createThrowable(ErrorHandler.java:216)\r\n\tat org.openqa.selenium.remote.ErrorHandler.throwIfResponseFailed(ErrorHandler.java:168)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:635)\r\n\tat org.openqa.selenium.remote.RemoteWebElement.execute(RemoteWebElement.java:274)\r\n\tat org.openqa.selenium.remote.RemoteWebElement.sendKeys(RemoteWebElement.java:98)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\r\n\tat java.lang.reflect.Method.invoke(Method.java:498)\r\n\tat org.openqa.selenium.support.pagefactory.internal.LocatingElementHandler.invoke(LocatingElementHandler.java:51)\r\n\tat com.sun.proxy.$Proxy16.sendKeys(Unknown Source)\r\n\tat pageObjects.HomePage.enterProductName(HomePage.java:106)\r\n\tat stepDefinitions.SearchSteps.enter_product_name_in_search_box(SearchSteps.java:20)\r\n\tat ✽.When Enter product name \"Dell\" \"Mac\"  in search box(search.feature:12)\r\n",
+  "status": "failed"
 });
 formatter.match({
   "location": "SearchSteps.click_Search_Button()"
 });
 formatter.result({
-  "duration": 18926,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "location": "SearchSteps.laptop_items_appear()"
 });
 formatter.result({
-  "duration": 95334226,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.after({
-  "duration": 137303092,
+  "duration": 3314104208,
   "status": "passed"
 });
 formatter.before({
-  "duration": 4778230620,
+  "duration": 13619778925,
   "status": "passed"
 });
 formatter.scenario({
@@ -217,7 +216,7 @@ formatter.match({
   "location": "SearchSteps.i_am_in_Home_Page()"
 });
 formatter.result({
-  "duration": 920192,
+  "duration": 1327807,
   "status": "passed"
 });
 formatter.match({
@@ -234,25 +233,24 @@ formatter.match({
   "location": "SearchSteps.enter_product_name_in_search_box(String,String)"
 });
 formatter.result({
-  "duration": 3500291343,
-  "status": "passed"
+  "duration": 129766778,
+  "error_message": "org.openqa.selenium.WebDriverException: unknown error: call function result missing \u0027value\u0027\n  (Session info: chrome\u003d71.0.3578.98)\n  (Driver info: chromedriver\u003d2.34.522940 (1a76f96f66e3ca7b8e57d503b4dd3bccfba87af1),platform\u003dWindows NT 10.0.17134 x86_64) (WARNING: The server did not provide any stacktrace information)\nCommand duration or timeout: 72 milliseconds\nBuild info: version: \u00273.0.1\u0027, revision: \u00271969d75\u0027, time: \u00272016-10-18 09:49:13 -0700\u0027\nSystem info: host: \u0027DESKTOP-LCVS2C2\u0027, ip: \u0027192.168.131.2\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_191\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities [{applicationCacheEnabled\u003dfalse, rotatable\u003dfalse, mobileEmulationEnabled\u003dfalse, networkConnectionEnabled\u003dfalse, chrome\u003d{chromedriverVersion\u003d2.34.522940 (1a76f96f66e3ca7b8e57d503b4dd3bccfba87af1), userDataDir\u003dC:\\Users\\MAHMUD~1\\AppData\\Local\\Temp\\scoped_dir40560_8778}, takesHeapSnapshot\u003dtrue, pageLoadStrategy\u003dnormal, databaseEnabled\u003dfalse, handlesAlerts\u003dtrue, hasTouchScreen\u003dfalse, version\u003d71.0.3578.98, platform\u003dXP, browserConnectionEnabled\u003dfalse, nativeEvents\u003dtrue, acceptSslCerts\u003dtrue, locationContextEnabled\u003dtrue, webStorageEnabled\u003dtrue, browserName\u003dchrome, takesScreenshot\u003dtrue, javascriptEnabled\u003dtrue, cssSelectorsEnabled\u003dtrue, setWindowRect\u003dtrue, unexpectedAlertBehaviour\u003d}]\nSession ID: c59954e9cf97ac70a0266563afee7106\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.openqa.selenium.remote.ErrorHandler.createThrowable(ErrorHandler.java:216)\r\n\tat org.openqa.selenium.remote.ErrorHandler.throwIfResponseFailed(ErrorHandler.java:168)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:635)\r\n\tat org.openqa.selenium.remote.RemoteWebElement.execute(RemoteWebElement.java:274)\r\n\tat org.openqa.selenium.remote.RemoteWebElement.sendKeys(RemoteWebElement.java:98)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\r\n\tat java.lang.reflect.Method.invoke(Method.java:498)\r\n\tat org.openqa.selenium.support.pagefactory.internal.LocatingElementHandler.invoke(LocatingElementHandler.java:51)\r\n\tat com.sun.proxy.$Proxy16.sendKeys(Unknown Source)\r\n\tat pageObjects.HomePage.enterProductName(HomePage.java:106)\r\n\tat stepDefinitions.SearchSteps.enter_product_name_in_search_box(SearchSteps.java:20)\r\n\tat ✽.When Enter product name \"Asus\" \"Hp\"  in search box(search.feature:12)\r\n",
+  "status": "failed"
 });
 formatter.match({
   "location": "SearchSteps.click_Search_Button()"
 });
 formatter.result({
-  "duration": 30056,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "location": "SearchSteps.laptop_items_appear()"
 });
 formatter.result({
-  "duration": 6523914,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.after({
-  "duration": 137427171,
+  "duration": 1381072352,
   "status": "passed"
 });
 });
